95878531e618cae87e82d368aac9da0f110691b0
Aryan Tailor <aryantailor.me@gmail.com>
2026-07-09 18:17:48 +0530

    Added solution - LeetHub

---
+++ 706296-count-pairs-divisible-by-k-GFG/706296-count-pairs-divisible-by-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countKdivPairs(self, arr, k):
+        freq = {}
+        ans = 0
+
+        for num in arr:
+            rem = num % k
+            need = (k - rem) % k
+
+            ans += freq.get(need, 0)
+            freq[rem] = freq.get(rem, 0) + 1
+
+        return ans
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
