c7e729ae9e38a0897096b2751710ab8b21fc89ed
Aryan Tailor <aryantailor.me@gmail.com>
2026-07-10 22:42:33 +0530

    Added solution - LeetHub

---
+++ 704548-ways-to-express-as-sum-of-consecutives-GFG/704548-ways-to-express-as-sum-of-consecutives.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCount(self, n):
+        count = 0
+        k = 2
+
+        while k * (k + 1) // 2 <= n:
+            num = n - k * (k - 1) // 2
+            if num > 0 and num % k == 0:
+                count += 1
+            k += 1
+
+        return count
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
